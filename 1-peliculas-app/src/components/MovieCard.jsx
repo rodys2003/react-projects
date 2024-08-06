7f53a7dc0c3edc1bc165f1012b197f16c51af9d3
rers <rodisenrique73@gmail.com>
2024-08-05 23:34:37 -0500

    logro: agregar link dirigido al componente de movieDetails, agregar componente getMovieImg para obtener la imagen de la pelicula

--- 1-peliculas-app/src/components/MovieCard.jsx
+++ 1-peliculas-app/src/components/MovieCard.jsx
@@ -1,5 +1,23 @@
+import "../components/MovieCard.css";
+import { Link } from "react-router-dom";
+import { getMovieImg } from "../utils/getMovieImg";
 
-export function MovieCard({movie}) {
-  const imageUrl = "http://image.tmdb.org/t/p/w300" + movie.poster_path
-  return <h1>movie card</h1>;
+export function MovieCard({ movie }) {
+
+  const imageUrl = getMovieImg( movie.poster_path, 300);
+
+  return (
+    <li className="movieCard">
+      <Link to={"movies/" + movie.id}>
+        <img
+          width={230}
+          height={345}
+          src={imageUrl}
+          alt={movie.title}
+          className="movieImage"
+        />
+      </Link>
+        <div>{movie.title}</div>
+    </li>
+  );
 }
